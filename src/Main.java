370133d3750ebd7ca193f7f819e1fc52101dff5c
burakguzell <102960797+mrputcher@users.noreply.github.com>
2022-10-06 16:10:18 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] list = {2, 3, 5, 2, 2, 7, 56, 78, 45, 56, 78, 99};
+        int[] dublicate = new int[list.length];
+        int startIndex = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if (i != j && (list[i] == list[j]) && (list[i] % 2 == 0)) {
+                    if (!(isFind(dublicate, list[i]))) {
+                        dublicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int value : dublicate) {
+            if (value != 0) {
+                System.out.println(value);
+            }
+        }
+    }
+}
